1238a54ea7d1d2252298db96f554cb821f822fb3
Jericho <jerichobantiquete@gmail.com>
2023-03-14 09:50:11 +0800

    array

---
+++ array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string fruits[] = {"apple", "banana", "mango"};
+
+    cout << fruits;
+
+    return 0;
+}
+
+
+// array - a data structure that can hold multiple values
+// values - are accessed by an index number
+// kind of like a variable that can hold multiple values
+
+
+// (a data structure is a data organization, management, and storage format that is usually chosen for efficient access to data. )
+
+
+
